64d8d0caca366f7b6c8c5b43afef1c07a57d0730
https://github.com/google/guava.wiki.git <conrad.marques.peres@gmail.com>
2017-03-30 11:38:45 -0300

    Cerberus sendo exposto por interface Rest em rotas apache camel via Bean do Spring Boot

--- src/main/java/com/redhat/SpringBootCamelXml/Application.java
+++ src/main/java/com/redhat/SpringBootCamelXml/Application.java
@@ -16,78 +16,79 @@
  */
 package com.redhat.SpringBootCamelXml;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.camel.spring.boot.FatJarRouter;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-
-import com.google.common.io.CharStreams;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-// load regular Spring XML file from the classpath that contains the Camel XML DSL
-@ImportResource({"classpath:spring/camel-context.xml"})
-public class Application extends FatJarRouter {
- 
-    @Override
-    public void configure() throws Exception {
-//    	from("netty-http:187.111.97.10:80/cerberus/seam/resource/v1/permissoes")
-//    	.setBody().simple("ref:helloWorld")
-//		.setHeader("ambiente", simple("DESENVOLVIMENTO", String.class))
-//		.setHeader("provedor", simple("EXEMPLO", String.class))
-//		.setHeader("consumidor", simple("EXEMPLO", String.class))
-//		.setHeader("chaveAcesso", simple("d4fec2a2-7f41-43bd-9c0c-c00afe8f4858", String.class))
-//		.setHeader("usuario", simple("05520685789", String.class))
-//		.setHeader("senhaUsuario", simple("", String.class));
-    	rest("/cerberus").get().to("direct:hello");
-		from("direct:hello").transform().simple("Hello Mundo!");
-    	rest("/bye").get().to("direct:bye");
-		from("direct:bye").transform().simple("Adios Mundo!");
-    	//rest("/cerberus").get().to("ref:cerberusBean");
-		//from("direct:bye").transform().simple("Adios Mundo!");
-    }
- 
-	// @Bean
-	// String helloWorld() {
-	// String responseAsString = null;
-	// HttpClient client = HttpClientBuilder.create().build();
-	// HttpGet request = new
-	// HttpGet("http://jeap.rio.rj.gov.br/cerberus/seam/resource/v1/permissoes");
-	// request.addHeader("ambiente", "DESENVOLVIMENTO");
-	// request.addHeader("provedor", "EXEMPLO");
-	// request.addHeader("consumidor", "EXEMPLO");
-	// request.addHeader("chaveAcesso", "d4fec2a2-7f41-43bd-9c0c-c00afe8f4858");
-	// request.addHeader("usuario", "05520685789");
-	// request.addHeader("senhaUsuario", "xxxx");
-	// HttpResponse response = null;
-	// try {
-	// response = client.execute(request);
-	//
-	// InputStream inputStream = response.getEntity().getContent();
-	// responseAsString = CharStreams.toString(new
-	// InputStreamReader(inputStream, "UTF-8"));
-	// System.out.println(response.getStatusLine().getStatusCode());
-	// System.out.println(responseAsString);
-	// } catch (IOException e) {
-	// e.printStackTrace();
-	// }
-	//
-	// return responseAsString;
-	// }
+// load regular Spring XML file from the classpath that contains the Camel XML
+// DSL
+@ImportResource({ "classpath:spring/camel-context.xml" })
+public class Application extends SpringBootServletInitializer {
+	@Bean
+	ServletRegistrationBean servletRegistrationBean() {
+		ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(),
+				"/cerberus/*");
+		servlet.setName("CamelServlet");
+		return servlet;
+	}
 
-    /**
-     * A main method to start this application.
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@Component
+	class RestApi extends RouteBuilder {
+		@Override
+		public void configure() throws Exception {
+			restConfiguration().contextPath("/cerberus").apiContextPath("/api-doc")
+					.apiProperty("api.title", "Camel REST API").apiProperty("api.version", "1.0")
+					.apiProperty("cors", "true").apiContextRouteId("doc-api").component("servlet")
+					.bindingMode(RestBindingMode.json);
+
+			rest("/bye").get().to("direct:bye");
+			from("direct:bye").bean(CerberusBean.class);
+
+		}
+
+		// @Bean
+		// String helloWorld() {
+		// String responseAsString = null;
+		// HttpClient client = HttpClientBuilder.create().build();
+		// HttpGet request = new
+		// HttpGet("http://jeap.rio.rj.gov.br/cerberus/seam/resource/v1/permissoes");
+		// request.addHeader("ambiente", "DESENVOLVIMENTO");
+		// request.addHeader("provedor", "EXEMPLO");
+		// request.addHeader("consumidor", "EXEMPLO");
+		// request.addHeader("chaveAcesso",
+		// "d4fec2a2-7f41-43bd-9c0c-c00afe8f4858");
+		// request.addHeader("usuario", "05520685789");
+		// request.addHeader("senhaUsuario", "xxxx");
+		// HttpResponse response = null;
+		// try {
+		// response = client.execute(request);
+		//
+		// InputStream inputStream = response.getEntity().getContent();
+		// responseAsString = CharStreams.toString(new
+		// InputStreamReader(inputStream, "UTF-8"));
+		// System.out.println(response.getStatusLine().getStatusCode());
+		// System.out.println(responseAsString);
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		//
+		// return responseAsString;
+		// }
+	}
+
+	/**
+	 * A main method to start this application.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
