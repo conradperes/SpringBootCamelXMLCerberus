f1570fcbe644c4259c9e83ab6b6ecc913feb96b8
https://github.com/google/guava.wiki.git <conrad.marques.peres@gmail.com>
2017-03-30 20:02:37 -0300

    produces application/json

--- src/main/java/com/redhat/SpringBootCamelXml/Application.java
+++ src/main/java/com/redhat/SpringBootCamelXml/Application.java
@@ -48,7 +48,7 @@ public class Application extends SpringBootServletInitializer {
 					.apiProperty("api.title", "Camel REST API").apiProperty("api.version", "1.0")
 					.apiProperty("cors", "true").apiContextRouteId("doc-api").component("servlet")
 					.bindingMode(RestBindingMode.json);
-			rest("/bye").description("Find user by usuario").get("/{usuario}").produces("text/plain").param().name("nome").
+			rest("/bye").description("Find user by usuario").get("/{usuario}").produces("application/json").param().name("nome").
 			type(RestParamType.path).description("primeiro valor a ser somado").dataType("String").endParam()
 			.to("bean:cerberusBean?method=getAuthorization(${headers.nome})");
 			// from("direct:bye").bean(CerberusBean.class);
